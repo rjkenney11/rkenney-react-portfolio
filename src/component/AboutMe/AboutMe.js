b6db09bffda833cc238b294d0e560fd48267b84c
rjkenney11 <rachelkenney@Rachels-MacBook-Pro.local>
2021-06-22 16:46:28 -0400

    ipad responsiveness

--- src/component/AboutMe/AboutMe.js
+++ src/component/AboutMe/AboutMe.js
@@ -6,8 +6,7 @@ import "./AboutMe.css";
 function AboutMe() {
   return (
         <div className="jumbotron">
-            <img src="https://user-images.githubusercontent.com/74163812/117556525-e402a300-b037-11eb-9a20-e169f72d4d6d.png"
-                className="card-img-top" alt="Rachel-Banner"></img>
+            <img className="card-img-top" src="https://user-images.githubusercontent.com/74163812/117556525-e402a300-b037-11eb-9a20-e169f72d4d6d.png" alt="Rachel-Banner"></img>
              <hr></hr>
             <hr></hr>
             <div className="container-text">
