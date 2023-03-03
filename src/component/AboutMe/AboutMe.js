c2628a576e175333b89bb610afdc2c0266300134
rjkenney11 <rachelkenney@Rachels-MBP.fios-router.home>
2023-03-02 21:44:19 -0500

    updated about me

--- src/component/AboutMe/AboutMe.js
+++ src/component/AboutMe/AboutMe.js
@@ -15,10 +15,10 @@ function AboutMe() {
                     src="https://user-images.githubusercontent.com/74163812/112669528-34ca8d80-8e36-11eb-886f-cecbebc1627e.png"
                     ALIGN="right" alt="a girl with red hair"></img>
                 <p className="lead">Hello and مرحبا !</p>
-                <p>My name is Rachel Kenney and I am considered by many to be the greatest Bop-It player of our time. </p>
-                <p>I graduated from the University of Maryland, College Park in 2015 with a Bachelor of Arts in Arabic Studies, but have always had a passion for design.</p> 
-                <p>I was recently given the opportunity to pursue a Full-Stack Web Development certification through Johns Hopkins University and successfully completed the course with an "A" average in May of 2021. I am eagerly looking forward to pursuing a career as a Full-Stack Web Developer as it scratches my creative itch, keeps my mind engaged and encourages me to be a lifelong learner.</p>
-                <p>If you look through my portfolio, you will find that I have a knack for creating fun user interactions, often incorporating my own designs or illustrations in the process.</p>
+                <p>My name is Rachel Kenney and I have a passion for design and creating fun user experiences, often incorporating self-made graphics and illustrations in the process. </p>
+                <p>I graduated from the University of Maryland, College Park in 2015 with a Bachelor of Arts in Arabic Studies and</p> 
+                <p>was recently given the opportunity to pursue a Full-Stack Web Development certification through Johns Hopkins University and successfully completed the course with an "A" average in May of 2021. I am eagerly looking forward to pursuing a career as a Full-Stack Web Developer as it scratches my creative itch, keeps my mind engaged and encourages me to be a lifelong learner.</p>
+                <p>If you look through my portfolio, you will find that I have a playful style with lots of reach-out-and-grab-you imagery.</p>
                     <p>A few facts about me:
                             <ul>
                             <li>Godzilla is considerd a Christmas movie in my household.</li>
