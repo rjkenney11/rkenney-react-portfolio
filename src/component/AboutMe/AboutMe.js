2ca59b34f72d8e4666718f4896391adb5945edb8
rjkenney11 <rachelkenney@Rachels-MBP.fios-router.home>
2023-03-02 21:48:09 -0500

    removing jokes

--- src/component/AboutMe/AboutMe.js
+++ src/component/AboutMe/AboutMe.js
@@ -18,14 +18,14 @@ function AboutMe() {
                 <p>My name is Rachel Kenney and I have a passion for design and creating fun user experiences, often incorporating self-made graphics and illustrations in the process. </p>
                 <p>I graduated from the University of Maryland, College Park in 2015 with a Bachelor of Arts in Arabic Studies and was recently given the opportunity to pursue a Full-Stack Web Development certification through Johns Hopkins University and successfully completed the course with an "A" average in May of 2021. I am eagerly looking forward to pursuing a career as a Full-Stack Web Developer as it scratches my creative itch, keeps my mind engaged and encourages me to be a lifelong learner.</p>
                 <p>If you look through my portfolio, you will find that I have a playful style with lots of reach-out-and-grab-you imagery.</p>
-                    <p>A few facts about me:
+                    {/* <p>A few facts about me:
                             <ul>
                             <li>Godzilla is considerd a Christmas movie in my household.</li>
                             <li>Nicolas Cage is my least favorite actor (and no "Leaving Las Vegas" will not change my mind).</li>
                             <li>I was voted "Most Fun" in my senior year of high school and will never let anyone forget it.
                                     </li>
                         </ul>
-                    </p>
+                    </p> */}
             </div>
         </div>
   );
