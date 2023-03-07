d4e45c9e2db5639feeb9a9aea602e26d1bd0978e
rjkenney11 <rachelkenney@Rachels-MBP.fios-router.home>
2023-03-06 21:05:33 -0500

    fonts

--- src/component/pages/About/About.js
+++ src/component/pages/About/About.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./About.css";
-import NavBar from "../../NavBar/NavBar";
+// import NavBar from "../../NavBar/NavBar";
 import Wrapper from "../../Wrapper/Wrapper";
 import AboutMe from "../../AboutMe/AboutMe";
 
 function About() {
   return (
     <div>
-    <NavBar /> 
+    {/* <NavBar />  */}
     <Wrapper>
       <AboutMe />
     </Wrapper>
